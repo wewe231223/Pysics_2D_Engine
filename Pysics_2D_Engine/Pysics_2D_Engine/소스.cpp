fecc9a5da453b51c4c1a1ae723880eb8a0de99ee
성준 김 <lastbabo2480@kpu.ac.kr>
2020-11-16 21:45:14 +0900

    WinAPI

--- Pysics_2D_Engine/Pysics_2D_Engine/소스.cpp
+++ Pysics_2D_Engine/Pysics_2D_Engine/소스.cpp
@@ -1,14 +1,68 @@
-#include <iostream>
+#include <tchar.h>
+#include <windows.h>
 
-using namespace std;
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+    static TCHAR szAppName[] = TEXT("HELLOWINDOWS");
+    HWND hWnd;
+    MSG msg;
+    WNDCLASS wndclass;
 
-int main(int argc, char* argv[]) {
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.lpszMenuName = NULL;
+    wndclass.lpszClassName = szAppName;
 
+    if (!RegisterClass(&wndclass))
+    {
+        MessageBox(NULL, TEXT("This program requires Windows 2000 or upper."),
+            szAppName, MB_OK | MB_ICONERROR);
+        return 0;
+    }
 
-	cout << "asdflkasdfasdf" << endl;
-	cout << "asdfasdfsdfasd" << endl;
-	cout << "12345678" << endl;
+    hWnd = CreateWindow(szAppName, TEXT("Hello Windows Application"),
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
 
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
-	return 1;
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    HDC hDC;
+    PAINTSTRUCT ps;
+    RECT rect;
+
+    switch (message)
+    {
+    case WM_CREATE:
+        return 0;
+    case WM_PAINT:
+        hDC = BeginPaint(hWnd, &ps);
+        GetClientRect(hWnd, &rect);
+        DrawText(hDC, TEXT("Hello, Windows!"), -1, &rect,
+            DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+        EndPaint(hWnd, &ps);
+        return 0;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
 }
